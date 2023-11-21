ae793330047cfcd07f4f9e76f5ac2ba17569ae86
Nikita <khramov-2016@inbox.ru>
2023-11-21 21:18:45 +0400

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public int calculate(double height, double weight) {
+         double index;
+        index = weight / (height * height);
+
+        return (int) index;
+    }
+}
+
